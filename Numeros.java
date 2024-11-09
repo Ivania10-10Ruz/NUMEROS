0e23f6902a943437c554774f16b49b19d7c6495b
Ivania10-10Ruz <ivaniaruz30@gmail.com>
2024-11-08 21:22:53 -0600

    Add files via upload

---
+++ Numeros.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numeros;
+
+/**
+ *
+ * @author Graciela
+ */
+public class Numeros {
+
+    public static void main(String[] args) {
+        System.out.println("Números pares del 2 al 100:");
+
+        // Usar un bucle for para imprimir los números pares
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
